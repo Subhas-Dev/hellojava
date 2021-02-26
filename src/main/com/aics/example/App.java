c592414109cf83db6e0c8371e1976bb81affd929
Subhas Luchmun <subhas.luchmun@gmail.com>
2021-02-26 16:43:10 -0500

    Initial Commit

---
+++ src/main/com/aics/example/App.java
@@ -0,0 +1,13 @@
+package com.aics.example;
+
+public class App {
+    private static final String GREETING = "Hello AICS.";
+
+    public String getGreeting() {
+        return GREETING;
+    }
+
+    public String testSonarWarning() {
+        return "Hi";
+    }
+}
